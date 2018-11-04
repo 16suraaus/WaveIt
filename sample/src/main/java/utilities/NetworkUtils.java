f66f0124c40fd7aa0a969f15f7c80b7b0d2da43b
Luke Song <csong1@nd.edu>
2018-11-04 01:12:34 -0500

    network util change

--- sample/src/main/java/utilities/NetworkUtils.java
+++ sample/src/main/java/utilities/NetworkUtils.java
@@ -3,15 +3,17 @@ package utilities;
 import android.net.Uri;
 import android.util.Log;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.Scanner;
 
 public class NetworkUtils {
-    static final String DRUGINFO_URL = "https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=207106";
+    static final String DRUGINFO_URL = "https://rxnav.nlm.nih.gov/REST/interaction/list.json?rxcuis=";
 
     public static URL buildUrl(String userQuery){
 
@@ -35,16 +37,28 @@ public class NetworkUtils {
     public static String getResponseFromHttpUrl(URL url) throws IOException{
         HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-        try {
-            InputStream in = urlConnection.getInputStream();
-            Scanner scanner = new Scanner(in);
-            scanner.useDelimiter("\\A");
+        //try {
+        //    InputStream in = urlConnection.getInputStream();
+        //    Scanner scanner = new Scanner(in);
+        //    scanner.useDelimiter("\\A");
 
-            boolean hasInput = scanner.hasNext();
-            if (hasInput) return scanner.next();
-            else return null;
-        } finally {
-            urlConnection.disconnect();
+        //    boolean hasInput = scanner.hasNext();
+        //    if (hasInput) return scanner.next();
+        //    else return null;
+        //} finally {
+        //    urlConnection.disconnect();
+        //}
+        BufferedReader br;
+        if (200 <= urlConnection.getResponseCode() && urlConnection.getResponseCode() <= 299) {
+            br = new BufferedReader(new InputStreamReader(urlConnection.getInputStream()));
+        } else {
+            br = new BufferedReader(new InputStreamReader(urlConnection.getErrorStream()));
         }
+        StringBuilder sb = new StringBuilder();
+        String output;
+        while ((output = br.readLine()) != null) {
+            sb.append(output);
+        }
+        return sb.toString();
     }   // end of method
 } // end of class
