357f088457150ed8c3cd1abe11d3e77f81e564e3
Andrew <sshin1@nd.edu>
2018-11-03 21:38:51 -0400

    added ordered insert and tuple

--- sample/src/main/java/example/zxing/scanner.java
+++ sample/src/main/java/example/zxing/scanner.java
@@ -1,6 +1,23 @@
 package example.zxing;
 import java.util.*;
 public class scanner {
-    List<String> scanned = new ArrayList<String>();
-    public void inputQR(String data) { scanned.add(data); }
+    ArrayList<String> scanned = new ArrayList<String>();
+    public class locCod<X,Y,Z> {
+        String X;
+        int Y;
+        String Z;
+    }
+    public boolean inputOrdered(String data){
+        this.scanned.add(data);
+        if(this.scanned.size()==0){
+            if(data.compareTo(this.scanned.get(this.scanned.size() - 1)) <0 ){
+                return false;
+            }
+        }
+        return true;
+    }
+    public boolean inpuutOrdered(String data){
+        this.scanned.add(data);
+
+    }
 }
