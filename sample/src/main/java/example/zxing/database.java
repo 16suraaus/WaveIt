9ea4760b5f9b363e892f9861eae095c27ea0b8aa
Andrew <sshin1@nd.edu>
2018-11-03 20:03:50 -0400

    database scanner

--- sample/src/main/java/example/zxing/database.java
+++ sample/src/main/java/example/zxing/database.java
@@ -1,3 +1,4 @@
+package example.zxing;
 import java.util.*;
 
 public class database{
--- sample/src/main/java/example/zxing/scanner.java
+++ sample/src/main/java/example/zxing/scanner.java
@@ -1,7 +1,6 @@
+package example.zxing;
 import java.util.*;
 public class scanner {
     List<String> scanned = new ArrayList<String>();
-    public void inputQR(String data) {
-        scanned.add(data);
-    }
+    public void inputQR(String data) { scanned.add(data); }
 }
